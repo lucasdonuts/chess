c39c9f20b2012fc31cd61c9cfbe500993b98cd67
Lucas <lucasdonuts@gmail.com>
2020-11-16 13:07:14 -0600

    Added and tested function to place pieces in starting positions

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
+
+
 class Board
   attr_reader :board
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
+    place_starting_pieces
   end
 
   def in_bounds?(move)
@@ -11,5 +14,41 @@ class Board
 
   def square_empty?(square)
     @board[square[0]][square[1]].nil?
+  end
+
+  def place_starting_pieces
+    @board[0][1] = Pawn.new('white')
+    @board[1][1] = Pawn.new('white')
+    @board[2][1] = Pawn.new('white')
+    @board[3][1] = Pawn.new('white')
+    @board[4][1] = Pawn.new('white')
+    @board[5][1] = Pawn.new('white')
+    @board[6][1] = Pawn.new('white')
+    @board[7][1] = Pawn.new('white')
+    @board[0][0] = Rook.new('white')
+    @board[7][0] = Rook.new('white')
+    @board[1][0] = Knight.new('white')
+    @board[6][0] = Knight.new('white')
+    @board[2][0] = Bishop.new('white')
+    @board[5][0] = Bishop.new('white')
+    @board[3][0] = Queen.new('white')
+    @board[4][0] = King.new('white')
+
+    @board[0][6] = Pawn.new('black')
+    @board[1][6] = Pawn.new('black')
+    @board[2][6] = Pawn.new('black')
+    @board[3][6] = Pawn.new('black')
+    @board[4][6] = Pawn.new('black')
+    @board[5][6] = Pawn.new('black')
+    @board[6][6] = Pawn.new('black')
+    @board[7][6] = Pawn.new('black')
+    @board[0][7] = Rook.new('black')
+    @board[7][7] = Rook.new('black')
+    @board[1][7] = Knight.new('black')
+    @board[6][7] = Knight.new('black')
+    @board[2][7] = Bishop.new('black')
+    @board[5][7] = Bishop.new('black')
+    @board[3][7] = Queen.new('black')
+    @board[4][7] = King.new('black')
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,10 +3,93 @@ require './lib/board.rb'
 describe Board do
   board = Board.new
   context "starting state of board" do
-    describe "#initialize"
+    describe "#initialize" do
       it "should create an array" do
         expect(board.board).to be_a(Array)
       end
+    end
+
+    describe "#place_starting_pieces" do
+      it "should place white pawns in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[0][1]).to be_an_instance_of(Pawn)
+        expect(board.board[1][1]).to be_an_instance_of(Pawn)
+        expect(board.board[2][1]).to be_an_instance_of(Pawn)
+        expect(board.board[3][1]).to be_an_instance_of(Pawn)
+        expect(board.board[4][1]).to be_an_instance_of(Pawn)
+        expect(board.board[5][1]).to be_an_instance_of(Pawn)
+        expect(board.board[6][1]).to be_an_instance_of(Pawn)
+        expect(board.board[7][1]).to be_an_instance_of(Pawn)
+      end
+
+      it "should place white rooks in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[0][0]).to be_an_instance_of(Rook)
+        expect(board.board[7][0]).to be_an_instance_of(Rook)
+      end
+
+      it "should place white knights in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[1][0]).to be_an_instance_of(Knight)
+        expect(board.board[6][0]).to be_an_instance_of(Knight)
+      end
+
+      it "should place white bishops in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[2][0]).to be_an_instance_of(Bishop)
+        expect(board.board[5][0]).to be_an_instance_of(Bishop)
+      end
+
+      it "should place white queen in correct starting position" do
+        board.place_starting_pieces
+        expect(board.board[3][0]).to be_an_instance_of(Queen)
+      end
+
+      it "should place white king in correct starting position" do
+        board.place_starting_pieces
+        expect(board.board[4][0]).to be_an_instance_of(King)
+      end
+
+      it "should place black pawns in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[0][6]).to be_an_instance_of(Pawn)
+        expect(board.board[1][6]).to be_an_instance_of(Pawn)
+        expect(board.board[2][6]).to be_an_instance_of(Pawn)
+        expect(board.board[3][6]).to be_an_instance_of(Pawn)
+        expect(board.board[4][6]).to be_an_instance_of(Pawn)
+        expect(board.board[5][6]).to be_an_instance_of(Pawn)
+        expect(board.board[6][6]).to be_an_instance_of(Pawn)
+        expect(board.board[7][6]).to be_an_instance_of(Pawn)
+      end
+
+      it "should place black rooks in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[0][0]).to be_an_instance_of(Rook)
+        expect(board.board[7][0]).to be_an_instance_of(Rook)
+      end
+
+      it "should place black knights in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[1][0]).to be_an_instance_of(Knight)
+        expect(board.board[6][0]).to be_an_instance_of(Knight)
+      end
+
+      it "should place black bishops in correct starting positions" do
+        board.place_starting_pieces
+        expect(board.board[2][0]).to be_an_instance_of(Bishop)
+        expect(board.board[5][0]).to be_an_instance_of(Bishop)
+      end
+
+      it "should place black queen in correct starting position" do
+        board.place_starting_pieces
+        expect(board.board[3][0]).to be_an_instance_of(Queen)
+      end
+
+      it "should place black king in correct starting position" do
+        board.place_starting_pieces
+        expect(board.board[4][0]).to be_an_instance_of(King)
+      end
+    end
   end
 
   context "validating moves" do
