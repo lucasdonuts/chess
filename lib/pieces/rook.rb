eed3f316b72aca44f5507e47658bb89328c0edc3
Lucas <lucasdonuts@gmail.com>
2020-11-19 16:02:47 -0600

    Finished and tested Rook move set

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -13,4 +13,68 @@ class Rook < Piece
   def select_symbol
     @color == :white ? '♜' : '♖'
   end
+
+  def get_moves(board)
+    moves = up_moves(board) + down_moves(board) + left_moves(board) + right_moves(board)
+  end
+
+  def up_moves(board)
+    x = @location[0]
+    up_moves = []
+
+    (@location[1] + 1).upto(7) do |i|
+      if !board.board[x][i].nil?
+        board.board[x][i].color == @color ? break : up_moves << [x, i]
+        break
+      else
+        up_moves << [x, i]
+      end
+    end
+    up_moves
+  end
+
+  def down_moves(board)
+    x = @location[0]
+    down_moves = []
+
+    (@location[1] - 1).downto(0) do |i|
+      if !board.board[x][i].nil?
+        board.board[x][i].color == @color ? break : down_moves << [x, i]
+        break
+      else
+        down_moves << [x, i]
+      end
+    end
+    down_moves
+  end
+
+  def left_moves(board)
+    y = @location[1]
+    left_moves = []
+
+    (@location[0] - 1).downto(0) do |i|
+      if !board.board[i][y].nil?
+        board.board[i][y].color == @color ? break : left_moves << [i, y]
+        break
+      else
+        left_moves << [i, y]
+      end
+    end
+    left_moves
+  end
+
+  def right_moves(board)
+    y = @location[1]
+    right_moves = []
+
+    (@location[0] + 1).upto(7) do |i|
+      if !board.board[i][y].nil?
+        board.board[i][y].color == @color ? break : right_moves << [i, y]
+        break
+      else
+        right_moves << [i, y]
+      end
+    end
+    right_moves
+  end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,9 +1,9 @@
 require './lib/pieces/rook.rb'
 
 describe Rook do
-  white_rook = Rook.new(:white, [0, 0])
-  black_rook = Rook.new(:black, [0, 7])
   describe "#initialize" do
+    white_rook = Rook.new(:white, [0, 0])
+    black_rook = Rook.new(:black, [0, 7])
 
     it "should create a rook piece with the color white" do
       expect(white_rook.color).to eq(:white)
@@ -19,6 +19,102 @@ describe Rook do
 
     it "should assign ♖ as the black rook's symbol" do
       expect(black_rook.symbol).to eq('♖')
+    end
+  end
+
+  describe "#up_moves" do
+    board = Board.new
+    white_rook = board.board[0][0]
+    it "should return no moves when blocked in by friendly pieces" do
+      expect(white_rook.up_moves(board)).to eq([])
+    end
+
+    it "should include enemy locations on it's path" do
+      board.board[0][1] = nil
+      expect(white_rook.up_moves(board)).to include([0, 6])
+    end
+
+    it "should stop adding moves when reaching a non-empty square" do
+      expect(white_rook.up_moves(board)).not_to include([0, 7])
+    end
+
+    it "should not return moves that are out of bounds" do
+      board.board[0][6] = nil
+      board.board[0][7] = nil
+      actual = board.board[0][0].up_moves(board).any? { |move| move.any? {|i| i < 0 || i > 7 } }
+      expect(actual).to be false
+    end
+  end
+
+  describe "#down_moves" do
+    board = Board.new
+    black_rook = board.board[0][7]
+    it "should return no moves when blocked in by friendly pieces" do
+      expect(black_rook.down_moves(board)).to eq([])
+    end
+
+    it "should include enemy locations on it's path" do
+      board.board[0][6] = nil
+      expect(black_rook.down_moves(board)).to include([0, 1])
+    end
+
+    it "should stop adding moves when reaching a non-empty square" do
+      expect(black_rook.down_moves(board)).not_to include([0, 0])
+    end
+
+    it "should not return moves that are out of bounds" do
+      board.board[0][1] = nil
+      board.board[0][0] = nil
+      actual = black_rook.down_moves(board).any? { |move| move.any? {|i| i < 0 || i > 7 } }
+      expect(actual).to be false
+    end
+  end
+
+  describe "#left_moves" do
+    board = Board.new
+    board.board[5][3] = Rook.new(:white, [5, 3])
+    board.board[1][3] = Rook.new(:black, [1, 3])
+    board.display_board
+    it "should return no moves when blocked in by friendly pieces" do
+      expect(board.board[7][0].left_moves(board)).to eq([])
+    end
+
+    it "should include enemy locations on it's path" do
+      expect(board.board[5][3].left_moves(board)).to include([1, 3])
+    end
+
+    it "should stop adding moves when reaching a non-empty square" do
+      expect(board.board[5][3].left_moves(board)).not_to include([0, 3])
+    end
+
+    it "should not return moves that are out of bounds" do
+      board.board[1][3] = nil
+      actual = board.board[5][3].left_moves(board).any? { |move| move.any? {|i| i < 0 || i > 7 } }
+      expect(actual).to be false
+    end
+  end
+
+  describe "#right_moves" do
+    board = Board.new
+    board.board[5][3] = Rook.new(:white, [5, 3])
+    board.board[1][3] = Rook.new(:black, [1, 3])
+    board.display_board
+    it "should return no moves when blocked in by friendly pieces" do
+      expect(board.board[0][7].right_moves(board)).to eq([])
+    end
+
+    it "should include enemy locations on it's path" do
+      expect(board.board[1][3].right_moves(board)).to include([5, 3])
+    end
+
+    it "should stop adding moves when reaching a non-empty square" do
+      expect(board.board[1][3].right_moves(board)).not_to include([6, 3], [7, 3])
+    end
+
+    it "should not return moves that are out of bounds" do
+      board.board[5][3] = nil
+      actual = board.board[1][3].right_moves(board).any? { |move| move.any? {|i| i < 0 || i > 7 } }
+      expect(actual).to be false
     end
   end
 end
