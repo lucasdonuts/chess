700cb353c7146b27bc79a97aab347263c6d96c46
Lucas <lucasdonuts@gmail.com>
2020-11-25 09:49:39 -0600

    temp

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -15,27 +15,33 @@ class King < Piece
     @color == :white ? '♚' : '♔'
   end
 
-  def get_moves
+  def get_moves(board)
     moves = []
     x = @location[0]
     y = @location[1]
 
-    moves << [x, y + 1]  
-    moves << [x, y - 1]  
-    moves << [x + 1, y]  
-    moves << [x + 1, y + 1]
-    moves << [x + 1, y - 1]
-    moves << [x - 1, y]  
-    moves << [x - 1, y + 1]
-    moves << [x - 1, y - 1]
+    moves << [x, y + 1] unless board.check_square([x, y + 1], @color) == 'friendly'
+    moves << [x, y - 1] unless board.check_square([x, y - 1], @color) == 'friendly'
+    moves << [x + 1, y] unless board.check_square([x + 1, y], @color) == 'friendly'
+    moves << [x + 1, y + 1] unless board.check_square([x + 1, y + 1], @color) == 'friendly'
+    moves << [x + 1, y - 1] unless board.check_square([x + 1, y - 1], @color) == 'friendly'
+    moves << [x - 1, y] unless board.check_square([x - 1, y], @color) == 'friendly'
+    moves << [x - 1, y + 1] unless board.check_square([x - 1, y + 1], @color) == 'friendly'
+    moves << [x - 1, y - 1] unless board.check_square([x - 1, y - 1], @color) == 'friendly'
 
-    moves += castling_moves
+    moves += castling_moves(board)
 
     moves.reject! {|move| move.any? {|i| i < 0 || i > 7 }}
     moves
   end
 
-  def castling_moves
-   
+  def castling_moves(board)
+    castling_moves = []
+    return [] if !board.can_castle_left?(self) and !board.can_castle_right?(self)
+    x = @location[0]
+    y = @location[1]
+    
+    castling_moves << [x - 2, y] if board.can_castle_left?(self)
+    castling_moves << [x + 2, y] if board.can_castle_right?(self)
   end
 end
